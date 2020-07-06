286b2fc28c6565b23670321e5371ece3c31c1cf0
Jessica Liang <jessicursliang@gmail.com>
2020-07-06 14:45:49 -0700

    setup router

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Home = () => {
+	return (
+		<p>
+			Hello bingus.
+		</p>
+	);
+};
+
+export default Home;
---
+++ src/components/MapPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { GoogleMap, Marker } from 'react-google-maps';
+
+const MapPage = () => {
+	return (
+		<p>
+			Hello mapo.
+		</p>
+	);
+};
+
+export default MapPage;
