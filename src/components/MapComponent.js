f76f8dcea228c22c7c52232c6d9c42479f2459bd
Jessica Liang <jessicursliang@gmail.com>
2020-07-06 17:52:07 -0700

    switch to mapbox

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -1,18 +1,9 @@
 import React from 'react';
+import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
 import PropTypes from 'prop-types';
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
-import mapStyles from './mapStyles.json';
 
-const MapComponent = withScriptjs(withGoogleMap((props) =>
-		<GoogleMap
-			defaultZoom={2}
-			defaultCenter={{ lat: 39.850034, lng: -176.058742 }}
-			defaultOptions={{ styles: mapStyles }}
-		>
-			{/* {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />} */}
-		</GoogleMap>
-));
+const MapComponent = ReactMapboxGl({
+	accessToken: process.env.REACT_APP_MAP_API
+});
 
 export default MapComponent;
-{/* <MyMapComponent isMarkerShown />// Map with a Marker
-<MyMapComponent isMarkerShown={false} />// Just only Map */}
--- src/components/MapPage.js
+++ src/components/MapPage.js
@@ -3,22 +3,22 @@ import PropTypes from 'prop-types';
 import MapComponent from './MapComponent';
 
 const MapPage = () => {
-	const BASE_URL = "https://maps.googleapis.com/maps/api/js?key=" + process.env.REACT_APP_MAP_API + "&map_ids=1de442a0ec3aad9";
 
 	return (
 		<div>
 			<MapComponent 
-				// isMarkerShown
-				googleMapURL={BASE_URL}
-				loadingElement={<div style={{ height: `100%` }} />}
-				containerElement={<div style={{ height: `400px` }} />}
-				mapElement={<div style={{ height: `100%` }} />}
+				style='mapbox://styles/jessicaliang/ckcb79d7d5vit1jq928rxsova'
+				containerStyle={{
+					height: '30vh',
+					width: '50vw'
+				}}
 			/>
 			<p>
 				Hello mapo.
 			</p>
 		</div>
 	);
+	
 };
 
 export default MapPage;
