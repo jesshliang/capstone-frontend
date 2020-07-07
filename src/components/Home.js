f9490f569d3d7e5c984576c56107c38f16493d21
Jessica Liang <jessicursliang@gmail.com>
2020-07-07 14:43:17 -0700

    setup router

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 const Home = () => {
 	return (
 		<p>
+			<nav>
+				<ul>
+					<li>
+						<Link to="/map-page">Map</Link>
+					</li>
+				</ul>
+			</nav>
+
 			Hello bingus.
 		</p>
 	);
--- src/components/MapPage.js
+++ src/components/MapPage.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import MapComponent from './MapComponent';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 const MapPage = () => {
 
 	return (
 		<div>
+			<nav>
+				<ul>
+					<li>
+						<Link to="/">Home</Link>
+					</li>
+					{/* <li>
+						<Link to="/map-page">Map</Link>
+					</li> */}
+				</ul>
+			</nav>
 			<MapComponent />
 			<p>
 				Hello mapo.
