ac8a8093ac96655ed561da4c67ad3333e54dbf4c
Jessica Liang <jessicursliang@gmail.com>
2020-07-07 14:10:10 -0700

    add test marker

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState, useRef } from 'react';
 import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
 import mapboxgl from "mapbox-gl";
-// import "mapbox-gl/dist/mapbox-gl.css";
+import 'mapbox-gl/dist/mapbox-gl.css'
 import PropTypes from 'prop-types';
 
 const styles = {
   width: "100vw",
-  height: "calc(100vh - 80px)",
+	height: "calc(100vh - 80px)",
+	overflow: "hidden",
   position: "absolute"
 };
 
@@ -20,17 +21,21 @@ const MapComponent = () => {
       const map = new mapboxgl.Map({
         container: mapContainer.current,
         style: "mapbox://styles/jessicaliang/ckcb79d7d5vit1jq928rxsova", // stylesheet location
-        center: [0, 0],
-        zoom: 1
-      });
-
+        center: [12.550343, 55.665957],
+        zoom: 8
+			});
+		
       map.on("load", () => {
-        setMap(map);
-        map.resize();
-      });
+				setMap(map);
+				map.resize();
+				
+				const marker = new mapboxgl.Marker()
+				.setLngLat([12.550343, 55.665957])
+				.addTo(map);
+			});
     };
 
-    if (!map) initializeMap({ setMap, mapContainer });
+		if (!map) initializeMap({ setMap, mapContainer });
   }, [map]);
 
   return <div ref={el => (mapContainer.current = el)} style={styles} />;
