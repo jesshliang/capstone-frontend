cff8548f762f3ebd2f7dfacfc26f180b93bd96e1
Jessica Liang <jessicursliang@gmail.com>
2020-07-13 15:06:47 -0700

    form seems complete

--- src/components/NewTripForm.js
+++ src/components/NewTripForm.js
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
+import Dropzone from "react-dropzone";
 import PropTypes from 'prop-types';
 
 const NewTripForm = () => {
 	
+	const [otherFields, setOtherFields] = useState({
+		title: '',
+		month: '',
+		year: ''
+	});
 	const [places, setPlaces] = useState([]);
 
 	const addPlaceField = (event) => {
 		event.preventDefault();
 		const newPlaces = [...places];
-		newPlaces.push("");
+		newPlaces.push(["", ""]);
 		setPlaces(newPlaces);
 	}
 
@@ -19,24 +25,45 @@ const NewTripForm = () => {
 		setPlaces(newPlaces);
 	}
 
-	const onFieldUpdate = (event, index) => {
+	const onPlacesUpdate = (event, index) => {
 		const updatedPlaces = [...places];
-		updatedPlaces[index] = event.target.value;
+		updatedPlaces[index][0] = event.target.value;
+		setPlaces(updatedPlaces);
+		console.log(places);
+	}
+
+	const onPlacesUrlUpdate = (event, index) => {
+		const updatedPlaces = [...places];
+		updatedPlaces[index][1] = event.target.value;
 		setPlaces(updatedPlaces);
 		console.log(places);
 	}
 	
+	const onOtherFieldsUpdate = (event) => {
+		const newOtherFields = {...otherFields};
+		newOtherFields[event.target.name] = event.target.value;
+		setOtherFields(newOtherFields);
+		console.log(otherFields);
+	}
+
+	const onAddNewTrip = (event) => {
+		event.preventDefault();
+		console.log(places);
+		console.log(otherFields);
+	}
+
 	return(
 		<div>
 			<h2>Add a New Trip</h2>
-			<form>
+			<form onSubmit={ onAddNewTrip }>
 				<label>
 					Title:<br />
-					<input type="text" />
+					<input type="text" value={ otherFields["title"] } name="title" onChange={ onOtherFieldsUpdate }/>
 				</label>
 				<label>
 					Date:<br />
-					<input type="text" placeholder="month" /> / <input type="text" placeholder="year" />
+					<input type="text" placeholder="month" value={ otherFields["month"] } name="month" onChange={ onOtherFieldsUpdate }/> / 
+					<input type="text" placeholder="year" value={ otherFields["year"] } name="year" onChange={ onOtherFieldsUpdate } />
 				</label>
 				<label>
 					Places:<br />
@@ -44,7 +71,8 @@ const NewTripForm = () => {
 						places.map((place, index) => {
 							return (
 								<section key={ index }>
-									<input key={ index } type="text" value={ place } onChange={(e) => onFieldUpdate(e, index) } />
+									<input type="text" value={ place[0] } onChange={(e) => onPlacesUpdate(e, index) } />
+									<input type="text" value={ place[1] } onChange={(e) => onPlacesUrlUpdate(e, index) } />
 									<button onClick = {(e) => removePlaceField(e, index) }>X</button>
 								</section>
 							);
@@ -53,7 +81,7 @@ const NewTripForm = () => {
 				</label>
 					<br />
 					<button onClick={ addPlaceField }>Add Place</button>
-					<input type="submit" value="Add New Trip" />
+					<input type="submit" value="Add New Trip" onSubmit={ onAddNewTrip } />
 				</form>
 		</div>
 	);
